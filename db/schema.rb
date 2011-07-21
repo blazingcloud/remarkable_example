070abcede0b6bc67dc9d0a188143f5bba996541e
Sarah Allen <sarah@ultrasaurus.com>
2011-07-21 09:01:50 -0700

    person model, pending spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110721155833) do
+
+  create_table "people", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
