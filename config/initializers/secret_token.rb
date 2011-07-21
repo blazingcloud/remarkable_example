6d5526d168849c42179eb02163fa4e2b8641e99d
Sarah Allen <sarah@ultrasaurus.com>
2011-07-21 08:33:49 -0700

    rails new remarkable_app -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemarkableApp::Application.config.secret_token = '2cba1c5172265067466fde1ef1eba742d27c29ce516056b3224988d3f5413750534445fe7176e589c305217fd19028d52e0bf56cb9f96d35a2d4efc05a0037ed'
